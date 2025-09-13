61f8da8c340d768d3caf4957f74269f936f9879e
Luke Zhao <me@lkzhao.com>
2025-09-12 20:57:03 -0700

    add test target

---
+++ RushDefenseTests/RushDefenseTests.swift
@@ -0,0 +1,16 @@
+//
+//  RushDefenseTests.swift
+//  RushDefenseTests
+//
+//  Created by Luke Zhao on 9/12/25.
+//
+
+import Testing
+
+struct RushDefenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
